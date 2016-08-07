141a9156ddb6d21ee85d6f960950a9d60eb4b531
ATPractice <atpractice2911@gmail.com>
2016-08-07 21:01:06 +0530

    #2 Added header component and recipes component and bootstrap css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   moduleId: module.id,
   selector: 'rb-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  directives: [ HeaderComponent ]
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-recipes',
+  templateUrl: 'recipes.component.html',
+  styleUrls: ['recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
